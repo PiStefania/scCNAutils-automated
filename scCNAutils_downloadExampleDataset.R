e60cc053a6203d59a351bb688f4bc7204c395cba
Stefania Patsou <stefanoula-fani@hotmail.com>
2021-06-12 15:03:38 +0300

    Update scCNAutils_downloadExampleDataset.R by adding a message

--- scCNAutils_downloadExampleDataset.R
+++ scCNAutils_downloadExampleDataset.R
@@ -5,3 +5,9 @@ setwd(pathProjectExample)
 system(
   'curl "https://singlecell.broadinstitute.org/single_cell/api/v1/search/bulk_download?accessions=SCP10&auth_code=ON9CKpB1&directory=all"  -o cfg.txt; curl -K cfg.txt && rm cfg.txt'
 )
+pathProjectExampleDataset = path(pathProjectExample, "SCP10")
+if(!dir.exists(pathProjectExampleDataset)) {
+  stop("Example dataset could not be downloaded.")
+} else {
+  message("Downloaded example dataset successfully.")
+}
